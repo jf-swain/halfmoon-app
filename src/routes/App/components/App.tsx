2253deebff7a599a177f39ce25fbd97fdf2fb47d
Jean-François Swain <jfswain@icloud.com>
2022-07-19 15:46:42 +0200

    Fix[App] - Remove error console due to the a tag without href

--- src/routes/App/components/App.tsx
+++ src/routes/App/components/App.tsx
@@ -4,49 +4,61 @@ import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 
 function App() {
-    return (
-        <div className="app">
-            <div className="level">
-                <div>
-                    <h1 className="title">Isalid Dark Mode</h1>
-                </div>
-
-                {/* --TODO: The button that should toggle dark mode-- */}
-                <button className="app__dark-mode-btn icon level-right">
-                    <FontAwesomeIcon icon={faMoon} />
-                </button>
-
-            </div>
-
-            <div className="columns">
-                <div className="column">
-                    <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
-                </div>
-                <div className="column">
-                    <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
-                </div>
-            </div>
-
-            <div className="field">
-                <div className="control">
-                    <input className="input" type="text" placeholder="Name" />
-                </div>
-            </div>
-
-            <div className="field">
-                <div className="control">
-                    <input className="input" type="text" placeholder="Email" />
-                </div>
-            </div>
-
-            <section className="section">
-                <div className="buttons level-right">
-                    <a className="button is-primary">Save</a>
-                    <a className="button is-link">Submit</a>
-                </div>
-            </section>
+  return (
+    <div className="app">
+      <div className="level">
+        <div>
+          <h1 className="title">Isalid Dark Mode</h1>
         </div>
-    );
+
+        {/* --TODO: The button that should toggle dark mode-- */}
+        <button className="app__dark-mode-btn icon level-right">
+          <FontAwesomeIcon icon={faMoon} />
+        </button>
+      </div>
+
+      <div className="columns">
+        <div className="column">
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Name" />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Email" />
+        </div>
+      </div>
+
+      <section className="section">
+        <div className="buttons level-right">
+          <button className="button is-primary">Save</button>
+          <button className="button is-link">Submit</button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
